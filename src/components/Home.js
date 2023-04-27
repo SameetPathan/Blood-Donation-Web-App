78d98e5ae22f3431b46a75fd0db2b821d7237717
SameetPathan <sameetpathanrs@gmail.com>
2023-04-27 21:33:06 +0530

    v3

--- src/components/Home.js
+++ src/components/Home.js
@@ -14,12 +14,12 @@ function Home() {
         <div class="card-body">
           <h5 class="card-title">Download You Android .apk file here and install <span><div class="blink">Download Available</div>
 </span></h5>
-         <a href="https://drive.google.com/uc?export=download&id=1YallFQrFC39CDMY955-oVv7P2SXn5rlM" class="btn btn-primary">Download</a>
+         <a href="https://drive.google.com/uc?export=download&id=162KSuH2hxjZaIxxOAOoS987vS6u9a_EG" class="btn btn-primary">Download</a>
    
 
         </div>
         <div class="card-footer text-muted">
-         Updated on 25/04/2023        </div>
+         Updated on 27/04/2023        </div>
       </div>
       </div>
 
